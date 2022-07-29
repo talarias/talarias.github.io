89e630bc7397634ea477d2b23947d15f0662e955
talarias <talarias@live.de>
2022-07-30 00:35:41 +0200

    add compontents and placeholders

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,32 +1,40 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
 import './slider.scss'
 
 const Slider: FC<any> = ({
   sliderHeader = null,
-  items
+  items,
+  siteTitle = null,
+  job = null,
+  children = null
 }) => {
+  const [active, setActive] = useState(false)
+  const toggle = () => {
+    setActive(!active)
+    console.log('asdasd')
+  }
+
   return (
-    <div className='slider'>
-      {sliderHeader
-        ? <div className='slider-header'>
-          <img src={sliderHeader} alt={'asd'}></img>
-        </div>
-        : null
-      }
-      <Carousel fade style={{ height: '400px' }}>
-        {
-          items.map((item:any, index:number) => {
+    <>
+      <div className={'slider ' + (active ? 'active' : '')}>
+        {sliderHeader
+          ? <div className='slider-header'>
+            <img src={sliderHeader} alt={'asd'}></img>
+          </div>
+          : null}
+        <Carousel fade style={{ minHeight: '400px' }} onSlide={toggle} onSlid={toggle} interval={8000}>
+          {items.map((item: any, index: number) => {
             return (
-              <Carousel.Item key={index} >
-                item
+              <Carousel.Item key={index}>
+                {item}
               </Carousel.Item>
             )
-          })
-        }
-      </Carousel>
-    </div>
+          })}
+        </Carousel>
+      </div>
+    </>
   )
 }
 
