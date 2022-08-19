5080231104f67d748b4c1ec41601c60e9c12c70a
talarias <talarias@live.de>
2022-08-20 01:29:44 +0200

    refactoring // add slider topics

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -5,33 +5,44 @@ import { SectionData } from './types'
 import './section.scss'
 
 const Section: FC<SectionData> = ({
-  title = 'Title',
   titleAction = null,
+  title,
+  simpleContent,
   children
 }) => {
   return (
     <>
         <Container>
-          <Row>
-            <Col>
-              <div className='section-item-wrapper'>
-                  <div className='section-item-title'>
-                      <div>
-                        <h2>{title}</h2>
-                        <div className='section-item-title-br'></div>
-                      </div>
-                      <div className='title-action'>
-                        {titleAction}
-                      </div>
-                  </div>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {children}
-            </Col>
-          </Row>
+          <>
+              {
+                title
+                  ? <Row>
+                  <Col>
+                    <div className='section-item-wrapper'>
+                        <div className='section-item-title'>
+                            <div>
+                              <h2>{title}</h2>
+                              <div className='section-item-title-br'></div>
+                            </div>
+                            <div className='title-action'>
+                              {titleAction}
+                            </div>
+                        </div>
+                    </div>
+                  </Col>
+                </Row>
+                  : ''
+              }
+            {
+              simpleContent
+                ? <Row>
+                    <Col>
+                      {children}
+                    </Col>
+                  </Row>
+                : <> { children } </>
+            }
+          </>
         </Container>
     </>
   )
