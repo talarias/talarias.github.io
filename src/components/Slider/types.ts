a39077b9d3830e03b398ba65bd657dc52362bb92
talarias <talarias@live.de>
2022-08-22 00:20:41 +0200

    refactoring slider

--- src/components/Slider/types.ts
+++ src/components/Slider/types.ts
@@ -1,4 +1,5 @@
 export class SliderData {
   items?: any
   topics?: String[]
+  interval?: number
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const items = [
   </>,
   <>
   <Section
-    title = 'asdasd'
+    title = 'Test'
     titleAction = {<Button variant="outline-primary">Primary</Button>}
     simpleContent = {true}
   >
@@ -39,11 +39,24 @@ const items = [
 </>,
   <>
   <Section
-    title = 'Skills'
+    title = 'Skills test'
     simpleContent = {true}
   >
     <Row>
       <Skills items = {mySkills} showFilter = {true}/>
+    </Row>
+  </Section>
+</>,
+<>
+  <Section
+    title = 'Test'
+    titleAction = {<Button variant="outline-primary">Primary</Button>}
+    simpleContent = {true}
+  >
+    <Row>
+      <Col><p>Im a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p></Col>
+      <Col><p>Im a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p></Col>
+      <Col><p>Im a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p></Col>
     </Row>
   </Section>
 </>
@@ -64,7 +77,8 @@ function Home () {
         />
         <Slider
           items = {items}
-          topics = {['t1', 't2', 't3']}
+          topics = {['About me', 'Test', 'Skills test', 'Test 2']}
+          interval = {0}
         />
         <PageRow variant = 'dark' >
           <Section
