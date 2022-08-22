223a6e5b50f8d7228b1257135177ae30feff64e1
talarias <talarias@live.de>
2022-08-22 21:46:00 +0200

    add header-brand rotate animation

--- src/components/HeaderBrand/Header.tsx
+++ src/components/HeaderBrand/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { HeaderBrandData } from './types'
 
 import './brand.scss'
@@ -6,10 +6,19 @@ import './brand.scss'
 const HeaderBrand: FC<HeaderBrandData> = ({
   frontImg
 }) => {
+  const [active, setActive] = useState(false)
+
+  const toggle = () => {
+    setActive(!active)
+  }
+
   return (
     <>
-      <div className='header-brand'>
-        <img src={frontImg} alt={'header-brand'}></img>
+      <div className={'header-brand flipped-' + active} onClick={toggle}>
+        <div className='card'>
+          <img src={frontImg} alt={'header-brand'} className='img-front'></img>
+          <img src={'/homeBg.jpeg'} alt={'header-brand'} className='img-back'></img>
+        </div>
       </div>
     </>
   )
